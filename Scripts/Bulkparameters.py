05344ff53882752fe85de94fdd90bbf46f14f14f
kanishkgits <kanishkcodes@gmail.com>
2025-07-08 14:53:39 +0530

    Added the alpha parameters and proton velocities.
    Bulkparameters now also takes in alpha parameters(bulk speed, thermal,
    density) and the proton velocities(x, y and z).

--- Scripts/Bulkparameters.py
+++ Scripts/Bulkparameters.py
@@ -3,39 +3,59 @@ from spacepy.pycdf import CDF
 import datetime as dt
 import glob
 
-def loadfiles(DATA_PATH, INVALID):
-    all_time = []
-    all_density = []
-    all_bulk_speed = []
-    all_thermal_speed = []
+def load_blk_variables(DATA_PATH, INVALID):
+    all_data = {
+        'time': [],
+        'proton_density': [],
+        'proton_bulk_speed': [],
+        'proton_thermal': [],
+        'alpha_density': [],
+        'alpha_bulk_speed': [],
+        'alpha_thermal': [],
+        'proton_xvelocity': [],
+        'proton_yvelocity': [],
+        'proton_zvelocity': [],
+    }
 
     for file in sorted(glob.glob(DATA_PATH)):
         with CDF(file) as cdf:
             try:
                 time = np.array([dt.datetime.fromtimestamp(e.timestamp(), dt.UTC) for e in cdf['epoch_for_cdf_mod'][:]])
-                density = np.array(cdf['proton_density'][:])
-                bulk_speed = np.array(cdf['proton_bulk_speed'][:])
-                thermal_speed = np.array(cdf['proton_thermal'][:])
+                variables = {
+                    'proton_density': np.array(cdf['proton_density'][:]),
+                    'proton_bulk_speed': np.array(cdf['proton_bulk_speed'][:]),
+                    'proton_thermal': np.array(cdf['proton_thermal'][:]),
+                    'alpha_density': np.array(cdf['alpha_density'][:]),
+                    'alpha_bulk_speed': np.array(cdf['alpha_bulk_speed'][:]),
+                    'alpha_thermal': np.array(cdf['alpha_thermal'][:]),
+                    'proton_xvelocity': np.array(cdf['proton_xvelocity'][:]),
+                    'proton_yvelocity': np.array(cdf['proton_yvelocity'][:]),
+                    'proton_zvelocity': np.array(cdf['proton_zvelocity'][:]),
+                }
             except KeyError as e:
                 print(f"Missing expected variable in {file}: {e}")
                 continue
 
-            # Filter invalid values
+            # Create a valid data mask (based on proton vars)
             valid_mask = (
-                (density != INVALID) &
-                (bulk_speed != INVALID) &
-                (thermal_speed != INVALID)
+                (variables['proton_density'] != INVALID) &
+                (variables['proton_bulk_speed'] != INVALID) &
+                (variables['proton_thermal'] != INVALID) &
+                (variables['alpha_density'] != INVALID) &
+                (variables['alpha_bulk_speed'] != INVALID) &
+                (variables['alpha_thermal'] != INVALID) &
+                (variables['proton_xvelocity'] != INVALID) &
+                (variables['proton_yvelocity'] != INVALID) &
+                (variables['proton_zvelocity'] != INVALID)
             )
 
-            # Apply mask and extend global lists
-            all_time.extend(time[valid_mask])
-            all_density.extend(density[valid_mask])
-            all_bulk_speed.extend(bulk_speed[valid_mask])
-            all_thermal_speed.extend(thermal_speed[valid_mask])
+            all_data['time'].extend(time[valid_mask])
+            for key in variables:
+                all_data[key].extend(variables[key][valid_mask])
 
-    all_time = np.array(all_time)
-    all_density = np.array(all_density)
-    all_bulk_speed = np.array(all_bulk_speed)
-    all_thermal_speed = np.array(all_thermal_speed)
+    # Convert all lists to NumPy arrays
+    for key in all_data:
+        all_data[key] = np.array(all_data[key])
 
-    return all_time, all_density, all_bulk_speed, all_thermal_speed
+    return all_data
+
